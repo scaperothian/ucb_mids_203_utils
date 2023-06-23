6fc93789fd28930781c86bbcfbc572a19fc85d8c
scaperot@vt.edu <scaperoth@vt.edu>
2023-06-23 18:41:13 -0400

    adding script to update repos.

---
+++ update_things.sh
@@ -0,0 +1,28 @@
+echo "Updating summer_23_central"
+echo "Currently in: `pwd`"
+cd ../summer_23_central
+git pull
+cd -
+
+echo ""
+echo "Updating Practice Problems"
+echo "Currently in: `pwd`"
+cd ../practice_problems
+git pull
+cd -
+
+echo ""
+echo "Updating Slides"
+echo "Currently in: `pwd`"
+cd ../slides
+git pull
+cd -
+
+
+echo ""
+echo "Updating Unit 7 Solutions"
+echo "Currently in: `pwd`"
+cd ../unit_07_ls_sol
+git pull
+cd -
+
